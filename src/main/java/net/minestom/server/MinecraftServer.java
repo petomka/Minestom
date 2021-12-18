bd0fa0bd1f13fe1d29a2474b39e5dbc200af65f1
mworzala <mattheworzala@gmail.com>
2021-12-18 09:53:14 -0500

    give extension manager a reference to relevant managers to avoid using MinecraftServer

--- src/main/java/net/minestom/server/MinecraftServer.java
+++ src/main/java/net/minestom/server/MinecraftServer.java
@@ -4,12 +4,17 @@ import net.minestom.server.advancements.AdvancementManager;
 import net.minestom.server.adventure.bossbar.BossBarManager;
 import net.minestom.server.command.CommandManager;
 import net.minestom.server.data.DataManager;
+import net.minestom.server.data.DataType;
+import net.minestom.server.data.SerializableData;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.exception.ExceptionManager;
+import net.minestom.server.extensions.Extension;
 import net.minestom.server.extensions.ExtensionManager;
+import net.minestom.server.fluid.Fluid;
 import net.minestom.server.gamedata.tags.TagManager;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.block.BlockManager;
+import net.minestom.server.instance.block.rule.BlockPlacementRule;
 import net.minestom.server.listener.manager.PacketListenerManager;
 import net.minestom.server.monitoring.BenchmarkManager;
 import net.minestom.server.network.ConnectionManager;
@@ -20,8 +25,10 @@ import net.minestom.server.network.socket.Server;
 import net.minestom.server.ping.ResponseDataConsumer;
 import net.minestom.server.recipe.RecipeManager;
 import net.minestom.server.scoreboard.TeamManager;
+import net.minestom.server.storage.StorageLocation;
 import net.minestom.server.storage.StorageManager;
-import net.minestom.server.thread.TickSchedulerThread;
+import net.minestom.server.terminal.MinestomTerminal;
+import net.minestom.server.thread.MinestomThreadPool;
 import net.minestom.server.timer.SchedulerManager;
 import net.minestom.server.utils.MathUtils;
 import net.minestom.server.utils.PacketUtils;
@@ -29,10 +36,8 @@ import net.minestom.server.utils.validate.Check;
 import net.minestom.server.world.Difficulty;
 import net.minestom.server.world.DimensionTypeManager;
 import net.minestom.server.world.biomes.BiomeManager;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.UnknownNullability;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,6 +63,10 @@ public final class MinecraftServer {
     public static final String THREAD_NAME_TICK_SCHEDULER = "Ms-TickScheduler";
     public static final String THREAD_NAME_TICK = "Ms-Tick";
 
+    public static final String THREAD_NAME_BLOCK_BATCH = "Ms-BlockBatchPool";
+    public static final int THREAD_COUNT_BLOCK_BATCH = getThreadCount("minestom.block-thread-count",
+            Runtime.getRuntime().availableProcessors() / 2);
+
     // Config
     // Can be modified at performance cost when increased
     public static final int TICK_PER_SECOND = Integer.getInteger("minestom.tps", 20);
@@ -66,9 +75,40 @@ public final class MinecraftServer {
     // Network monitoring
     private static int rateLimit = 300;
     private static int maxPacketSize = 30_000;
+    // Network
+    private static PacketListenerManager packetListenerManager;
+    private static PacketProcessor packetProcessor;
+    private static Server server;
+
+    private static ExceptionManager exceptionManager;
 
     // In-Game Manager
-    private static volatile ServerProcess serverProcess;
+    private static ConnectionManager connectionManager;
+    private static InstanceManager instanceManager;
+    private static BlockManager blockManager;
+    private static CommandManager commandManager;
+    private static RecipeManager recipeManager;
+    private static StorageManager storageManager;
+    private static DataManager dataManager;
+    private static TeamManager teamManager;
+    private static SchedulerManager schedulerManager;
+    private static BenchmarkManager benchmarkManager;
+    private static DimensionTypeManager dimensionTypeManager;
+    private static BiomeManager biomeManager;
+    private static AdvancementManager advancementManager;
+    private static BossBarManager bossBarManager;
+
+    private static ExtensionManager extensionManager;
+
+    private static final GlobalEventHandler GLOBAL_EVENT_HANDLER = new GlobalEventHandler();
+
+    private static UpdateManager updateManager;
+    private static MinecraftServer minecraftServer;
+
+    // Data
+    private static boolean initialized;
+    private static boolean started;
+    private static volatile boolean stopping;
 
     private static int chunkViewDistance = Integer.getInteger("minestom.chunk-view-distance", 8);
     private static int entityViewDistance = Integer.getInteger("minestom.entity-view-distance", 5);
@@ -77,22 +117,57 @@ public final class MinecraftServer {
     private static ResponseDataConsumer responseDataConsumer;
     private static String brandName = "Minestom";
     private static Difficulty difficulty = Difficulty.NORMAL;
+    private static TagManager tagManager;
 
     public static MinecraftServer init() {
-        updateProcess();
-        return new MinecraftServer();
-    }
+        if (minecraftServer != null) // don't init twice
+            return minecraftServer;
 
-    @ApiStatus.Internal
-    public static ServerProcess updateProcess() {
-        ServerProcess process;
+        // Initialize the ExceptionManager at first
+        exceptionManager = new ExceptionManager();
+
+        extensionManager = new ExtensionManager(exceptionManager, GLOBAL_EVENT_HANDLER);
+
+        // warmup/force-init registries
+        // without this line, registry types that are not loaded explicitly will have an internal empty registry in Registries
+        // That can happen with PotionType for instance, if no code tries to access a PotionType field
+        // TODO: automate (probably with code generation)
+        Fluid.values();
+
+        connectionManager = new ConnectionManager();
+        // Networking
+        packetProcessor = new PacketProcessor();
+        packetListenerManager = new PacketListenerManager();
+
+        instanceManager = new InstanceManager();
+        blockManager = new BlockManager();
+        commandManager = new CommandManager();
+        recipeManager = new RecipeManager();
+        storageManager = new StorageManager();
+        dataManager = new DataManager();
+        teamManager = new TeamManager();
+        schedulerManager = new SchedulerManager();
+        benchmarkManager = new BenchmarkManager();
+        dimensionTypeManager = new DimensionTypeManager();
+        biomeManager = new BiomeManager();
+        advancementManager = new AdvancementManager();
+        bossBarManager = new BossBarManager();
+
+        updateManager = new UpdateManager();
+
+        tagManager = new TagManager();
+
         try {
-            process = new ServerProcessImpl();
-            serverProcess = process;
+            server = new Server(packetProcessor);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-        return process;
+
+        initialized = true;
+
+        minecraftServer = new MinecraftServer();
+
+        return minecraftServer;
     }
 
     /**
@@ -172,51 +247,108 @@ public final class MinecraftServer {
         PacketUtils.broadcastPacket(new ServerDifficultyPacket(difficulty, true));
     }
 
-    @ApiStatus.Experimental
-    public static @UnknownNullability ServerProcess process() {
-        return serverProcess;
-    }
-
+    /**
+     * Gets the global event handler.
+     * <p>
+     * Used to register event callback at a global scale.
+     *
+     * @return the global event handler
+     */
     public static @NotNull GlobalEventHandler getGlobalEventHandler() {
-        return serverProcess.eventHandler();
+        return GLOBAL_EVENT_HANDLER;
     }
 
+    /**
+     * Gets the manager handling all incoming packets
+     *
+     * @return the packet listener manager
+     */
     public static PacketListenerManager getPacketListenerManager() {
-        return serverProcess.packetListener();
+        checkInitStatus(packetListenerManager);
+        return packetListenerManager;
     }
 
+    /**
+     * Gets the manager handling all registered instances.
+     *
+     * @return the instance manager
+     */
     public static InstanceManager getInstanceManager() {
-        return serverProcess.instance();
+        checkInitStatus(instanceManager);
+        return instanceManager;
     }
 
+    /**
+     * Gets the manager handling {@link net.minestom.server.instance.block.BlockHandler block handlers}
+     * and {@link BlockPlacementRule placement rules}.
+     *
+     * @return the block manager
+     */
     public static BlockManager getBlockManager() {
-        return serverProcess.block();
+        checkInitStatus(blockManager);
+        return blockManager;
     }
 
+    /**
+     * Gets the manager handling commands.
+     *
+     * @return the command manager
+     */
     public static CommandManager getCommandManager() {
-        return serverProcess.command();
+        checkInitStatus(commandManager);
+        return commandManager;
     }
 
+    /**
+     * Gets the manager handling recipes show to the clients.
+     *
+     * @return the recipe manager
+     */
     public static RecipeManager getRecipeManager() {
-        return serverProcess.recipe();
+        checkInitStatus(recipeManager);
+        return recipeManager;
     }
 
+    /**
+     * Gets the manager handling storage.
+     *
+     * @return the storage manager
+     */
     @Deprecated
     public static StorageManager getStorageManager() {
-        return serverProcess.storage();
+        checkInitStatus(storageManager);
+        return storageManager;
     }
 
+    /**
+     * Gets the manager handling {@link DataType} used by {@link SerializableData}.
+     *
+     * @return the data manager
+     */
     @Deprecated
     public static DataManager getDataManager() {
-        return serverProcess.data();
+        checkInitStatus(dataManager);
+        return dataManager;
     }
 
+    /**
+     * Gets the manager handling teams.
+     *
+     * @return the team manager
+     */
     public static TeamManager getTeamManager() {
-        return serverProcess.team();
+        checkInitStatus(teamManager);
+        return teamManager;
     }
 
+    /**
+     * Gets the manager handling scheduled tasks.
+     *
+     * @return the scheduler manager
+     */
     public static SchedulerManager getSchedulerManager() {
-        return serverProcess.scheduler();
+        checkInitStatus(schedulerManager);
+        return schedulerManager;
     }
 
     /**
@@ -225,31 +357,68 @@ public final class MinecraftServer {
      * @return the benchmark manager
      */
     public static BenchmarkManager getBenchmarkManager() {
-        return serverProcess.benchmark();
+        checkInitStatus(benchmarkManager);
+        return benchmarkManager;
     }
 
+    /**
+     * Gets the exception manager for exception handling.
+     *
+     * @return the exception manager
+     */
     public static ExceptionManager getExceptionManager() {
-        return serverProcess.exception();
+        checkInitStatus(exceptionManager);
+        return exceptionManager;
     }
 
+    /**
+     * Gets the manager handling server connections.
+     *
+     * @return the connection manager
+     */
     public static ConnectionManager getConnectionManager() {
-        return serverProcess.connection();
+        checkInitStatus(connectionManager);
+        return connectionManager;
     }
 
+    /**
+     * Gets the boss bar manager.
+     *
+     * @return the boss bar manager
+     */
     public static BossBarManager getBossBarManager() {
-        return serverProcess.bossBar();
+        checkInitStatus(bossBarManager);
+        return bossBarManager;
     }
 
+    /**
+     * Gets the object handling the client packets processing.
+     * <p>
+     * Can be used if you want to convert a buffer to a client packet object.
+     *
+     * @return the packet processor
+     */
     public static PacketProcessor getPacketProcessor() {
-        return serverProcess.packetProcessor();
+        checkInitStatus(packetProcessor);
+        return packetProcessor;
     }
 
+    /**
+     * Gets if the server is up and running.
+     *
+     * @return true if the server is started
+     */
     public static boolean isStarted() {
-        return serverProcess.isAlive();
+        return started;
     }
 
+    /**
+     * Gets if the server is currently being shutdown using {@link #stopCleanly()}.
+     *
+     * @return true if the server is being stopped
+     */
     public static boolean isStopping() {
-        return !isStarted();
+        return stopping;
     }
 
     /**
@@ -270,7 +439,7 @@ public final class MinecraftServer {
      */
     @Deprecated
     public static void setChunkViewDistance(int chunkViewDistance) {
-        Check.stateCondition(serverProcess.isAlive(), "You cannot change the chunk view distance after the server has been started.");
+        Check.stateCondition(started, "You cannot change the chunk view distance after the server has been started.");
         Check.argCondition(!MathUtils.isBetween(chunkViewDistance, 2, 32),
                 "The chunk view distance must be between 2 and 32");
         MinecraftServer.chunkViewDistance = chunkViewDistance;
@@ -294,7 +463,7 @@ public final class MinecraftServer {
      */
     @Deprecated
     public static void setEntityViewDistance(int entityViewDistance) {
-        Check.stateCondition(serverProcess.isAlive(), "You cannot change the entity view distance after the server has been started.");
+        Check.stateCondition(started, "You cannot change the entity view distance after the server has been started.");
         Check.argCondition(!MathUtils.isBetween(entityViewDistance, 0, 32),
                 "The entity view distance must be between 0 and 32");
         MinecraftServer.entityViewDistance = entityViewDistance;
@@ -318,7 +487,7 @@ public final class MinecraftServer {
      * @throws IllegalStateException if this is called after the server started
      */
     public static void setCompressionThreshold(int compressionThreshold) {
-        Check.stateCondition(serverProcess.isAlive(), "The compression threshold cannot be changed after the server has been started.");
+        Check.stateCondition(started, "The compression threshold cannot be changed after the server has been started.");
         MinecraftServer.compressionThreshold = compressionThreshold;
     }
 
@@ -337,7 +506,7 @@ public final class MinecraftServer {
      * @param enabled true to enable, false to disable
      */
     public static void setTerminalEnabled(boolean enabled) {
-        Check.stateCondition(serverProcess.isAlive(), "Terminal settings may not be changed after starting the server.");
+        Check.stateCondition(started, "Terminal settings may not be changed after starting the server.");
         MinecraftServer.terminalEnabled = enabled;
     }
 
@@ -349,31 +518,73 @@ public final class MinecraftServer {
      */
     @Deprecated
     public static ResponseDataConsumer getResponseDataConsumer() {
+        checkInitStatus(responseDataConsumer);
         return responseDataConsumer;
     }
 
+    /**
+     * Gets the manager handling dimensions.
+     *
+     * @return the dimension manager
+     */
     public static DimensionTypeManager getDimensionTypeManager() {
-        return serverProcess.dimension();
+        checkInitStatus(dimensionTypeManager);
+        return dimensionTypeManager;
     }
 
+    /**
+     * Gets the manager handling biomes.
+     *
+     * @return the biome manager
+     */
     public static BiomeManager getBiomeManager() {
-        return serverProcess.biome();
+        checkInitStatus(biomeManager);
+        return biomeManager;
     }
 
+    /**
+     * Gets the manager handling advancements.
+     *
+     * @return the advancement manager
+     */
     public static AdvancementManager getAdvancementManager() {
-        return serverProcess.advancement();
+        checkInitStatus(advancementManager);
+        return advancementManager;
     }
 
+    /**
+     * Get the manager handling {@link Extension}.
+     *
+     * @return the extension manager
+     */
     public static ExtensionManager getExtensionManager() {
-        return serverProcess.extension();
+        checkInitStatus(extensionManager);
+        return extensionManager;
     }
 
+    /**
+     * Gets the manager handling tags.
+     *
+     * @return the tag manager
+     */
     public static TagManager getTagManager() {
-        return serverProcess.tag();
+        checkInitStatus(tagManager);
+        return tagManager;
+    }
+
+    /**
+     * Gets the manager handling the server ticks.
+     *
+     * @return the update manager
+     */
+    public static UpdateManager getUpdateManager() {
+        checkInitStatus(updateManager);
+        return updateManager;
     }
 
     public static Server getServer() {
-        return serverProcess.server();
+        checkInitStatus(server);
+        return server;
     }
 
     /**
@@ -403,15 +614,65 @@ public final class MinecraftServer {
      * @throws IllegalStateException if called before {@link #init()} or if the server is already running
      */
     public void start(@NotNull String address, int port) {
-        serverProcess.start(new InetSocketAddress(address, port));
-        new TickSchedulerThread(serverProcess).start();
+        Check.stateCondition(!initialized, "#start can only be called after #init");
+        Check.stateCondition(started, "The server is already started");
+
+        extensionManager.start();
+        extensionManager.gotoPreInit();
+
+        MinecraftServer.started = true;
+
+        LOGGER.info("Starting Minestom server.");
+
+        updateManager.start();
+
+        extensionManager.gotoInit();
+
+        // Init server
+        try {
+            server.init(new InetSocketAddress(address, port));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Start server
+        server.start();
+
+        LOGGER.info("Minestom server started successfully.");
+
+        if (terminalEnabled) {
+            MinestomTerminal.start();
+        }
+
+        // Stop the server on SIGINT
+        Runtime.getRuntime().addShutdownHook(new Thread(MinecraftServer::stopCleanly));
     }
 
     /**
      * Stops this server properly (saves if needed, kicking players, etc.)
      */
     public static void stopCleanly() {
-        serverProcess.stop();
+        if (stopping) return;
+        stopping = true;
+        LOGGER.info("Stopping Minestom server.");
+        LOGGER.info("Unloading all extensions.");
+        extensionManager.shutdown();
+        updateManager.stop();
+        schedulerManager.shutdown();
+        connectionManager.shutdown();
+        server.stop();
+        storageManager.getLoadedLocations().forEach(StorageLocation::close);
+        LOGGER.info("Shutting down all thread pools.");
+        benchmarkManager.disable();
+        MinestomTerminal.stop();
+        MinestomThreadPool.shutdownAll();
+        LOGGER.info("Minestom server stopped successfully.");
+    }
+
+    private static void checkInitStatus(@Nullable Object object) {
+        /*Check.stateCondition(Objects.isNull(object),
+                "You cannot access the manager before MinecraftServer#init, " +
+                        "if you are developing an extension be sure to retrieve them at least after Extension#preInitialize");*/
     }
 
     private static int getThreadCount(@NotNull String property, int count) {
