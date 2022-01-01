b4e53b6b0f399611ba2273d34bf09f61c3beaa98
themode <themode@outlook.fr>
2022-01-01 22:29:39 +0100

    Add generateAll

--- src/main/java/net/minestom/server/instance/generator/Generator.java
+++ src/main/java/net/minestom/server/instance/generator/Generator.java
@@ -2,6 +2,12 @@ package net.minestom.server.instance.generator;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public interface Generator {
     @NotNull GenerationResponse generate(@NotNull GenerationRequest request);
+
+    default @NotNull List<GenerationResponse> generateAll(@NotNull List<GenerationRequest> requests) {
+        return requests.stream().map(this::generate).toList();
+    }
 }
