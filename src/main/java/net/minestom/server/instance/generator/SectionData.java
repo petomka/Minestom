b06e50aebca16a9437399b7571f27882dc922a39
themode <themode@outlook.fr>
2022-01-01 21:26:25 +0100

    Remove unnecessary classes

--- src/main/java/net/minestom/server/instance/generator/SectionData.java
+++
@@ -1,7 +0,0 @@
-package net.minestom.server.instance.generator;
-
-import net.minestom.server.instance.palette.Palette;
-import net.minestom.server.utils.block.SectionBlockCache;
-
-public record SectionData(SectionBlockCache blockCache, Palette biomePalette, int y) {
-}
--- src/main/java/net/minestom/server/instance/generator/SectionResult.java
+++
@@ -1,6 +0,0 @@
-package net.minestom.server.instance.generator;
-
-import net.minestom.server.coordinate.Point;
-
-public record SectionResult(SectionData sectionData, Point location) {
-}
