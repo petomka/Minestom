d49ae6fa7d12b23826de5cd6601ba90acb75150b
themode <themode@outlook.fr>
2022-01-02 18:57:56 +0100

    Add GenerationUnit#units

--- src/main/java/net/minestom/server/instance/generator/GenerationUnit.java
+++ src/main/java/net/minestom/server/instance/generator/GenerationUnit.java
@@ -5,7 +5,11 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public interface GenerationUnit {
-    @NotNull List<UnitProperty.Section> sections();
+    @NotNull List<UnitProperty> units();
+
+    interface Section extends GenerationUnit {
+        @NotNull List<UnitProperty.Section> sections();
+    }
 
     interface Chunk extends GenerationUnit {
         @NotNull List<UnitProperty.Chunk> chunks();
