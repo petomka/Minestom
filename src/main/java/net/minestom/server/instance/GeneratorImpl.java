77ccf6f5d6ec584003dd244d530ac666fd0ff89a
themode <themode@outlook.fr>
2022-01-02 19:14:26 +0100

    Implement fill

--- src/main/java/net/minestom/server/instance/GeneratorImpl.java
+++ src/main/java/net/minestom/server/instance/GeneratorImpl.java
@@ -28,52 +28,46 @@ final class GeneratorImpl {
 
         Map<Chunk, List<UnitProperty.Section>> chunkSectionsMap = new HashMap<>(chunks.size());
         for (Chunk chunk : chunks) {
-            record SectionImpl(int sectionX, int sectionY, int sectionZ, UnitModifier modifier)
+            record SectionImpl(int sectionX, int sectionY, int sectionZ,
+                               Point size, Point absoluteStart, Point absoluteEnd, UnitModifier modifier)
                     implements UnitProperty.Section {
             }
-            AtomicInteger sectionY = new AtomicInteger(minY);
+            AtomicInteger sectionCounterY = new AtomicInteger(minY);
             var sectionProperties = chunk.getSections().stream().map(section -> {
-                final UnitModifier modifier = new UnitModifier() {
-                    @Override
-                    public void fill(@NotNull Block block) {
-                        throw new UnsupportedOperationException();
-                    }
-
-                    @Override
-                    public void fill(@NotNull Point start, @NotNull Point end, @NotNull Block block) {
-                        throw new UnsupportedOperationException();
-                    }
-
+                final int sectionX = chunk.getChunkX();
+                final int sectionY = sectionCounterY.getAndIncrement();
+                final int sectionZ = chunk.getChunkZ();
+                final var size = new Vec(16, 16, 16);
+                final var start = new Vec(sectionX * 16, sectionY * 16, sectionZ * 16);
+                final var end = new Vec(sectionX * 16 + 16, sectionY * 16 + 16, sectionZ * 16 + 16);
+                final UnitModifier modifier = new ModifierImpl(start, end) {
                     @Override
                     public void setBlock(int x, int y, int z, @NotNull Block block) {
+                        if (x < start.x() || x >= end.x() || y < start.y() || y >= end.y() || z < start.z() || z >= end.z())
+                            return;
                         final int localX = ChunkUtils.toSectionRelativeCoordinate(x);
                         final int localY = ChunkUtils.toSectionRelativeCoordinate(y);
                         final int localZ = ChunkUtils.toSectionRelativeCoordinate(z);
                         section.blockPalette().set(localX, localY, localZ, block.stateId());
                     }
                 };
-                return new SectionImpl(chunk.getChunkX(), sectionY.getAndIncrement(), chunk.getChunkZ(), modifier);
+                return new SectionImpl(sectionX, sectionY, sectionZ, size, start, end, modifier);
             }).toList();
             chunkSectionsMap.put(chunk, (List) sectionProperties);
         }
         record Impl(int chunkX, int chunkZ, int minY, List<Section> sections,
-                    Point size, UnitModifier modifier)
+                    Point size, Point absoluteStart, Point absoluteEnd, UnitModifier modifier)
                 implements UnitProperty.Chunk {
         }
 
         final var result = chunks.stream().map(chunk -> {
             final var sections = chunkSectionsMap.get(chunk);
-            final UnitModifier modifier = new UnitModifier() {
-                @Override
-                public void fill(@NotNull Block block) {
-                    throw new UnsupportedOperationException();
-                }
-
-                @Override
-                public void fill(@NotNull Point start, @NotNull Point end, @NotNull Block block) {
-                    throw new UnsupportedOperationException();
-                }
-
+            final int chunkX = chunk.getChunkX();
+            final int chunkZ = chunk.getChunkZ();
+            final var size = new Vec(16, sizeY - minY, 16);
+            final var start = new Vec(chunkX * 16, minY, chunkZ * 16);
+            final var end = new Vec(chunkX * 16 + 16, size.y() + minY, chunkZ * 16 + 16);
+            final UnitModifier modifier = new ModifierImpl(start, end) {
                 @Override
                 public void setBlock(int x, int y, int z, @NotNull Block block) {
                     y -= minY;
@@ -86,7 +80,7 @@ final class GeneratorImpl {
                     section.modifier().setBlock(localX, localY, localZ, block);
                 }
             };
-            return new Impl(chunk.getChunkX(), chunk.getChunkZ(), minY, sections, new Vec(16, sizeY - minY, 16), modifier);
+            return new Impl(chunkX, chunkZ, minY, sections, size, start, end, modifier);
         }).toList();
         return (List) result;
     }
@@ -104,5 +98,36 @@ final class GeneratorImpl {
             }
         }
         return new Impl(s, c);
+    }
+
+    static abstract class ModifierImpl implements UnitModifier {
+        private final Point start, end;
+
+        public ModifierImpl(Point start, Point end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public void fill(@NotNull Block block) {
+            for (int x = start.blockX(); x < end.blockX(); x++) {
+                for (int y = start.blockY(); y < end.blockY(); y++) {
+                    for (int z = start.blockZ(); z < end.blockZ(); z++) {
+                        setBlock(x, y, z, block);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void fill(@NotNull Point start, @NotNull Point end, @NotNull Block block) {
+            for (int x = start.blockX(); x < end.blockX(); x++) {
+                for (int y = start.blockY(); y < end.blockY(); y++) {
+                    for (int z = start.blockZ(); z < end.blockZ(); z++) {
+                        setBlock(x, y, z, block);
+                    }
+                }
+            }
+        }
     }
 }
