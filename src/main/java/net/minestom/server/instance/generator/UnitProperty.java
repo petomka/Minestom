03c136126c26a8e8948541a3a539fca2cea8f78c
themode <themode@outlook.fr>
2022-01-02 02:54:52 +0100

    Add size to UnitProperty

--- src/main/java/net/minestom/server/instance/generator/UnitProperty.java
+++ src/main/java/net/minestom/server/instance/generator/UnitProperty.java
@@ -1,13 +1,67 @@
 package net.minestom.server.instance.generator;
 
 public interface UnitProperty {
-    interface Section {
+
+    int sizeX();
+
+    int sizeY();
+
+    int sizeZ();
+
+    interface Section extends UnitProperty {
         int absoluteHeight();
+
+        @Override
+        default int sizeX() {
+            return 16;
+        }
+
+        @Override
+        default int sizeY() {
+            return 16;
+        }
+
+        @Override
+        default int sizeZ() {
+            return 16;
+        }
     }
 
-    interface Chunk {
+    interface Chunk extends UnitProperty {
         int chunkX();
 
         int chunkZ();
+
+        @Override
+        default int sizeX() {
+            return 16;
+        }
+
+        @Override
+        int sizeY();
+
+        @Override
+        default int sizeZ() {
+            return 16;
+        }
+    }
+
+    interface Region extends UnitProperty {
+        int regionX();
+
+        int regionZ();
+
+        @Override
+        default int sizeX() {
+            return 512;
+        }
+
+        @Override
+        int sizeY();
+
+        @Override
+        default int sizeZ() {
+            return 512;
+        }
     }
 }
