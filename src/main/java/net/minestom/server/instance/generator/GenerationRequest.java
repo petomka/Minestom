ffdf259c7ea7de11d124e047a2e9feaffc4c5ed7
themode <themode@outlook.fr>
2022-01-02 21:18:14 +0100

    Add future to GenerationRequest

--- src/main/java/net/minestom/server/instance/generator/GenerationRequest.java
+++ src/main/java/net/minestom/server/instance/generator/GenerationRequest.java
@@ -3,6 +3,10 @@ package net.minestom.server.instance.generator;
 import net.minestom.server.instance.Instance;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.CompletableFuture;
+
 public interface GenerationRequest {
     @NotNull Instance instance();
+
+    void returnAsync(@NotNull CompletableFuture<?> future);
 }
