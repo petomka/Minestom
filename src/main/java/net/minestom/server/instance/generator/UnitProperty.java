66b763b019b64bc94a0c13991638166a9f3ce9fc
themode <themode@outlook.fr>
2022-01-02 19:14:26 +0100

    Implement fill

--- src/main/java/net/minestom/server/instance/generator/UnitProperty.java
+++ src/main/java/net/minestom/server/instance/generator/UnitProperty.java
@@ -1,7 +1,6 @@
 package net.minestom.server.instance.generator;
 
 import net.minestom.server.coordinate.Point;
-import net.minestom.server.coordinate.Vec;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnknownNullability;
 
@@ -22,21 +21,6 @@ public interface UnitProperty {
         int sectionY();
 
         int sectionZ();
-
-        @Override
-        default @NotNull Point size() {
-            return new Vec(16, 16, 16);
-        }
-
-        @Override
-        default @NotNull Point absoluteStart() {
-            return new Vec(sectionX() * 16, sectionY() * 16, sectionZ() * 16);
-        }
-
-        @Override
-        default @NotNull Point absoluteEnd() {
-            return new Vec(sectionX() * 16 + 16, sectionY() * 16 + 16, sectionZ() * 16 + 16);
-        }
     }
 
     interface Chunk extends UnitProperty {
@@ -44,22 +28,10 @@ public interface UnitProperty {
 
         int chunkZ();
 
-        int minY();
-
         @NotNull List<Section> sections();
 
         default @UnknownNullability Section section(int offset) {
             return sections().get(offset);
-        }
-
-        @Override
-        default @NotNull Point absoluteStart() {
-            return new Vec(chunkX() * 16, minY(), chunkZ() * 16);
-        }
-
-        @Override
-        default @NotNull Point absoluteEnd() {
-            return new Vec(chunkX() * 16 + 16, size().y() + minY(), chunkZ() * 16 + 16);
         }
     }
 
