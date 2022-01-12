d3b98b1c8e70da5355103bdfff25286e86dca3dd
themode <themode@outlook.fr>
2022-01-12 11:23:01 +0100

    Assert unit sizes

--- src/main/java/net/minestom/server/instance/generator/GenerationUnit.java
+++ src/main/java/net/minestom/server/instance/generator/GenerationUnit.java
@@ -12,8 +12,17 @@ public interface GenerationUnit {
 
     @NotNull Point absoluteEnd();
 
-    static GenerationUnit unit(UnitModifier modifier, Point absoluteStart, Point absoluteEnd) {
-        final Point size = absoluteEnd.sub(absoluteStart);
+    static GenerationUnit unit(UnitModifier modifier, Point start, Point end) {
+        if (start.x() > end.x() || start.y() > end.y() || start.z() > end.z()) {
+            throw new IllegalArgumentException("absoluteStart must be before absoluteEnd");
+        }
+        if (start.x() % 16 != 0 || start.y() % 16 != 0 || start.z() % 16 != 0) {
+            throw new IllegalArgumentException("absoluteStart must be a multiple of 16");
+        }
+        if (end.x() % 16 != 0 || end.y() % 16 != 0 || end.z() % 16 != 0) {
+            throw new IllegalArgumentException("absoluteEnd must be a multiple of 16");
+        }
+        final Point size = end.sub(start);
         return new GenerationUnit() {
             @Override
             public @NotNull UnitModifier modifier() {
@@ -27,12 +36,12 @@ public interface GenerationUnit {
 
             @Override
             public @NotNull Point absoluteStart() {
-                return absoluteStart;
+                return start;
             }
 
             @Override
             public @NotNull Point absoluteEnd() {
-                return absoluteEnd;
+                return end;
             }
         };
     }
