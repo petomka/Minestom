0abc7873508c73bd9b44121ecb8639019fa2246c
themode <themode@outlook.fr>
2022-01-12 14:39:59 +0100

    Remove instance from request

--- src/main/java/net/minestom/server/instance/generator/GenerationRequest.java
+++ src/main/java/net/minestom/server/instance/generator/GenerationRequest.java
@@ -1,13 +1,10 @@
 package net.minestom.server.instance.generator;
 
-import net.minestom.server.instance.Instance;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
 public interface GenerationRequest {
-    @NotNull Instance instance();
-
     void returnAsync(@NotNull CompletableFuture<?> future);
 
     @NotNull GenerationUnit unit();
